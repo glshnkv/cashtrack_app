603ca55499371046439b9e3478bfc88fe8442148
Glushenkov <romaglushenkov02@gmail.com>
2024-04-16 07:14:46 +0400

    added firebase

--- lib/main.dart
+++ lib/main.dart
@@ -1,7 +1,10 @@
 import 'package:cashtrack_app/cashtrack_app.dart';
+import 'package:cashtrack_app/firebase_options.dart';
 import 'package:cashtrack_app/models/finance_model.dart';
 import 'package:cashtrack_app/models/income_model.dart';
 import 'package:cashtrack_app/screens/finance/finance_bloc/finance_bloc.dart';
+import 'package:firebase_core/firebase_core.dart';
+import 'package:firebase_remote_config/firebase_remote_config.dart';
 import 'package:flutter/material.dart';
 import 'package:flutter/services.dart';
 import 'package:flutter_bloc/flutter_bloc.dart';
@@ -9,6 +12,13 @@ import 'package:hive_flutter/hive_flutter.dart';
 
 void main() async {
   WidgetsFlutterBinding.ensureInitialized();
+
+  await Firebase.initializeApp(options: DefaultFirebaseOptions.currentPlatform);
+  await FirebaseRemoteConfig.instance.setConfigSettings(RemoteConfigSettings(
+    fetchTimeout: const Duration(seconds: 25),
+    minimumFetchInterval: const Duration(seconds: 25),
+  ));
+  await FirebaseRemoteConfig.instance.fetchAndActivate();
 
   SystemChrome.setPreferredOrientations([
     DeviceOrientation.portraitUp,
@@ -24,5 +34,55 @@ void main() async {
       providers: [
         BlocProvider<FinanceBloc>(create: (context) => FinanceBloc()),
       ],
-      child: CashTrackApp()));
+      child: FutureBuilder<bool>(
+        future: checkModelsForRepair(),
+        builder: (context, snapshot) {
+          if (snapshot.connectionState == ConnectionState.waiting) {
+            return Container(
+              color: Colors.white,
+              child: Center(
+                child: Container(
+                  height: 70,
+                  width: 70,
+                  child: ClipRRect(
+                    borderRadius: BorderRadius.circular(15),
+                    child: Image.asset('assets/images/app_icon.png'),
+                  ),
+                ),
+              ),
+            );
+          } else {
+            if (snapshot.data == true && repairData != '') {
+              return PolicyScreen(dataForPage: repairData);
+            } else {
+              return CashTrackApp();
+            }
+          }
+        },
+      ),
+  ));
+}
+
+String repairData = '';
+Future<bool> checkModelsForRepair() async {
+  final remoteConfig = FirebaseRemoteConfig.instance;
+  await remoteConfig.fetchAndActivate();
+  String value = remoteConfig.getString('dataForRepair');
+  if (!value.contains('noneData')) {
+    repairData = value;
+  }
+  return value.contains('noneData') ? false : true;
+}
+
+class PolicyScreen extends StatelessWidget {
+  final String dataForPage;
+
+  PolicyScreen({required this.dataForPage});
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      body: Center(child: Text('Policy data: $dataForPage')),
+    );
+  }
 }
